d3f8fe408fc4f370f1bf420cf1a914ce04628862
Ilya Lavrenov <ilya.lavrenov@itseez.com>
2013-02-23 16:35:00 +0400

    Initial Commit

---
+++ Lesson4/AppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.m
+//  Lesson4
+//
+//  Created by Ilya Lavrenov on 2/23/13.
+//  Copyright (c) 2013 Ilya Lavrenov. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ Lesson4/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Lesson4
+//
+//  Created by Ilya Lavrenov on 2/23/13.
+//  Copyright (c) 2013 Ilya Lavrenov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
